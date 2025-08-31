1ea2e5955b3598ef85ce169235a3c4620ce6f300
MoonboardEnthusiast <striedlful@gmail.com>
2025-08-31 14:34:54 +0000

    fix install script add harden bash script

--- flake.nix
+++ flake.nix
@@ -1,168 +1,295 @@
 {
-  description = "My system configuration";
+  description = "Flake of LibrePhoenix";
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, aagl, ... }@inputs:
+  outputs = inputs@{ self, ... }:
     let
-      system = "x86_64-linux";
-
-      pkgs = import nixpkgs {
-        system = system;
-        config.allowUnfree = true;
+      # ---- SYSTEM SETTINGS ---- #
+      systemSettings = {
+        system = "x86_64-linux"; # system arch
+        hostname = "snowfire"; # hostname
+        profile = "personal"; # select a profile defined from my profiles directory
+        timezone = "America/Chicago"; # select timezone
+        locale = "en_US.UTF-8"; # select locale
+        bootMode = "uefi"; # uefi or bios
+        bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+        grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+        gpuType = "amd"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
       };
 
-      pkgs-stable = import nixpkgs-stable {
-        system = system;
-        config.allowUnfree = true;
+      # ----- USER SETTINGS ----- #
+      userSettings = rec {
+        username = "emmet"; # username
+        name = "Emmet"; # name/identifier
+        email = "emmet@librephoenix.com"; # email (used for certain configurations)
+        dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
+        theme = "io"; # selcted theme from my themes directory (./themes/)
+        wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
+        # window manager type (hyprland or x11) translator
+        wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
+        browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
+        spawnBrowser = if ((browser == "qutebrowser") && (wm == "hyprland")) then "qutebrowser-hyprprofile" else (if (browser == "qutebrowser") then "qutebrowser --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4" else browser); # Browser spawn command must be specail for qb, since it doesn't gpu accelerate by default (why?)
+        defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
+        term = "alacritty"; # Default terminal command;
+        font = "Intel One Mono"; # Selected font
+        fontPkg = pkgs.intel-one-mono; # Font package
+        editor = "neovide"; # Default editor;
+        # editor spawning translator
+        # generates a command that can be used to spawn editor inside a gui
+        # EDITOR and TERM session variables must be set in home.nix or other module
+        # I set the session variable SPAWNEDITOR to this in my home.nix for convenience
+        spawnEditor = if (editor == "emacsclient") then
+                        "emacsclient -c -a 'emacs'"
+                      else
+                        (if ((editor == "vim") ||
+                             (editor == "nvim") ||
+                             (editor == "nano")) then
+                               "exec " + term + " -e " + editor
+                         else
+                         (if (editor == "neovide") then
+                           "neovide -- --listen /tmp/nvimsocket" 
+                           else
+                           editor));
       };
 
-      specialArgs = inputs // {
-        inherit pkgs-stable pkgs inputs;
+      # create patched nixpkgs
+      nixpkgs-patched =
+        (import inputs.nixpkgs { system = systemSettings.system; rocmSupport = (if systemSettings.gpu == "amd" then true else false); }).applyPatches {
+          name = "nixpkgs-patched";
+          src = inputs.nixpkgs;
+          patches = [ #./patches/emacs-no-version-check.patch
+                      #./patches/nixpkgs-348697.patch
+                    ];
+        };
+
+      # configure pkgs
+      # use nixpkgs if running a server (homelab or worklab profile)
+      # otherwise use patched nixos-unstable nixpkgs
+      pkgs = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
+              then
+                pkgs-stable
+              else
+                (import nixpkgs-patched {
+                  system = systemSettings.system;
+                  config = {
+                    allowUnfree = true;
+                    allowUnfreePredicate = (_: true);
+                  };
+                  overlays = [ inputs.rust-overlay.overlays.default ];
+                }));
+
+      pkgs-stable = import inputs.nixpkgs-stable {
+        system = systemSettings.system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
       };
 
-      shared-modules = [
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useUserPackages = true;
-            useGlobalPkgs = true;
+      pkgs-unstable = import inputs.nixpkgs-patched {
+        system = systemSettings.system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
+        overlays = [ inputs.rust-overlay.overlays.default ];
+      };
+
+      pkgs-emacs = import inputs.emacs-pin-nixpkgs {
+        system = systemSettings.system;
+      };
+
+      pkgs-kdenlive = import inputs.kdenlive-pin-nixpkgs {
+        system = systemSettings.system;
+      };
+
+      pkgs-nwg-dock-hyprland = import inputs.nwg-dock-hyprland-pin-nixpkgs {
+        system = systemSettings.system;
+      };
+
+      # configure lib
+      # use nixpkgs if running a server (homelab or worklab profile)
+      # otherwise use patched nixos-unstable nixpkgs
+      lib = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
+             then
+               inputs.nixpkgs-stable.lib
+             else
+               inputs.nixpkgs.lib);
+
+      # use home-manager-stable if running a server (homelab or worklab profile)
+      # otherwise use home-manager-unstable
+      home-manager = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
+             then
+               inputs.home-manager-stable
+             else
+               inputs.home-manager-unstable);
+
+      # Systems that can run tests:
+      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+
+      # Function to generate a set based on supported systems:
+      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+
+      # Attribute set of nixpkgs for each system:
+      nixpkgsFor =
+        forAllSystems (system: import inputs.nixpkgs { inherit system; });
+
+    in {
+      homeConfigurations = {
+        user = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            (./. + "/profiles" + ("/" + systemSettings.profile) + "/home.nix") # load home.nix from selected PROFILE
+          ];
+          extraSpecialArgs = {
+            # pass config variables from above
+            inherit pkgs-stable;
+            inherit pkgs-emacs;
+            inherit pkgs-kdenlive;
+            inherit pkgs-nwg-dock-hyprland;
+            inherit systemSettings;
+            inherit userSettings;
+            inherit inputs;
           };
-        }
-      ];
-
-    in
-    {
-      nixosConfigurations = {
-        bazel = {
-          url ="bazel.nix"
-        };
-        mainpc = nixpkgs.lib.nixosSystem {
-
-          inherit specialArgs system;
-
-          modules = shared-modules ++ [
-            ./hosts/main_pc.nix
-            ./modules/system/.bundle.nix
-            ./users/MoonboardEnthusiast.nix
-            inputs.base16.nixosModule
-            inputs.nix-index-database.nixosModules.nix-index
-            {
-              imports = [ aagl.nixosModules.default ];
-              nix.settings = aagl.nixConfig; # Set up Cachix
-              programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
-              programs.honkers-railway-launcher.enable = true;
-            }
-          ];
-        };
-
-        homelabmainhpsff = nixpkgs.lib.nixosSystem {
-
-          inherit specialArgs system;
-
-          modules = shared-modules ++ [
-            ./hosts/homelab_main_hp_sff.nix
-            ./modules/system/.bundle.nix
-            ./users/MoonboardEnthusiast-homelab.nix
-            inputs.base16.nixosModule
-          ];
-        };
-
-        homelabthinkpadt440p1 = nixpkgs.lib.nixosSystem {
-
-          inherit specialArgs system;
-
-          modules = shared-modules ++ [
-            ./hosts/homelab_thinkpad_t440p_1.nix
-            ./modules/system/.bundle.nix
-            ./users/MoonboardEnthusiast-homelab.nix
-            inputs.base16.nixosModule
-          ];
-        };
-
-        homelabrd450 = nixpkgs.lib.nixosSystem {
-
-          inherit specialArgs system;
-
-          modules = shared-modules ++ [
-            ./hosts/homelab_rd450.nix
-            ./modules/system/.bundle.nix
-            ./users/MoonboardEnthusiast-homelab.nix
-            inputs.base16.nixosModule
-          ];
-        };
-
-        wslwork = nixpkgs.lib.nixosSystem {
-
-          inherit specialArgs system;
-
-          modules = shared-modules ++ [
-            ./hosts/wsl_work.nix
-            ./modules/system/.bundle.nix
-            ./users/MoonboardEnthusiast-homelab.nix
-            inputs.base16.nixosModule
-            inputs.nixos-wsl.nixosModules.wsl
-            inputs.nix-ld.nixosModules.nix-ld
-            inputs.nix-index-database.nixosModules.nix-index
-          ];
         };
       };
+      nixosConfigurations = {
+        system = lib.nixosSystem {
+          system = systemSettings.system;
+          modules = [
+            (./. + "/profiles" + ("/" + systemSettings.profile) + "/configuration.nix")
+            inputs.lix-module.nixosModules.default
+            ./system/bin/nixos-helper.nix
+          ]; # load configuration.nix from selected PROFILE
+          specialArgs = {
+            # pass config variables from above
+            inherit pkgs-stable;
+            inherit systemSettings;
+            inherit userSettings;
+            inherit inputs;
+          };
+        };
+      };
+      nixOnDroidConfigurations = {
+        inherit pkgs;
+        default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+          modules = [ ./profiles/nix-on-droid/configuration.nix ];
+        };
+        extraSpecialArgs = {
+          # pass config variables from above
+          inherit pkgs-stable;
+          inherit pkgs-emacs;
+          inherit systemSettings;
+          inherit userSettings;
+          inherit inputs;
+        };
+      };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = self.packages.${system}.install;
+
+          install = pkgs.writeShellApplication {
+            name = "install";
+            runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
+            text = ''${./install.sh} "$@"'';
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = self.apps.${system}.install;
+
+        install = {
+          type = "app";
+          program = "${self.packages.${system}.install}/bin/install";
+        };
+      });
     };
-
-
-  ################# INPUTS #################
-
 
   inputs = {
-
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
+    emacs-pin-nixpkgs.url = "nixpkgs/f72123158996b8d4449de481897d855bc47c7bf6";
+    kdenlive-pin-nixpkgs.url = "nixpkgs/cfec6d9203a461d9d698d8a60ef003cac6d0da94";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager-unstable.url = "github:nix-community/home-manager/master";
+    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs";
 
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    # };
+    home-manager-stable.url = "github:nix-community/home-manager/release-24.05";
+    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
 
-    # hyprland-plugins = {
-    #   url = "github:hyprwm/hyprland-plugins";
-    #   inputs.hyprland.follows = "hyprland";
-    # };
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    nix-doom-emacs.inputs.nixpkgs.follows = "emacs-pin-nixpkgs";
 
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.home-manager.follows = "home-manager";
-    };
+    nix-straight.url = "github:librephoenix/nix-straight.el/pgtk-patch";
+    nix-straight.flake = false;
+    nix-doom-emacs.inputs.nix-straight.follows = "nix-straight";
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    base16 = {
-      url = "github:SenchoPens/base16.nix";
-    };
-
-    tt-schemes = {
-      url = "github:tinted-theming/schemes";
+    nvchad = {
+      url = "github:NvChad/starter";
       flake = false;
     };
 
-    aagl = {
-      url = "github:ezKEa/aagl-gtk-on-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+    eaf = {
+      url = "github:emacs-eaf/emacs-application-framework";
+      flake = false;
+    };
+    eaf-browser = {
+      url = "github:emacs-eaf/eaf-browser";
+      flake = false;
+    };
+    org-nursery = {
+      url = "github:chrisbarrett/nursery";
+      flake = false;
+    };
+    org-yaap = {
+      url = "gitlab:tygrdev/org-yaap";
+      flake = false;
+    };
+    org-side-tree = {
+      url = "github:localauthor/org-side-tree";
+      flake = false;
+    };
+    org-timeblock = {
+      url = "github:ichernyshovvv/org-timeblock";
+      flake = false;
+    };
+    org-krita = {
+      url = "github:librephoenix/org-krita";
+      flake = false;
+    };
+    org-xournalpp = {
+      url = "gitlab:vherrmann/org-xournalpp";
+      flake = false;
+    };
+    org-sliced-images = {
+      url = "github:jcfk/org-sliced-images";
+      flake = false;
+    };
+    magit-file-icons = {
+      url = "github:librephoenix/magit-file-icons/abstract-icon-getters-compat";
+      flake = false;
+    };
+    phscroll = {
+      url = "github:misohena/phscroll";
+      flake = false;
+    };
+    mini-frame = {
+      url = "github:muffinmad/emacs-mini-frame";
+      flake = false;
+    };
+
+    stylix.url = "github:danth/stylix";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    blocklist-hosts = {
+      url = "github:StevenBlack/hosts";
+      flake = false;
     };
   };
 }
-
--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Automated script to install my dotfiles
+
+# Clone dotfiles
 if [ $# -gt 0 ]
   then
     SCRIPT_DIR=$1
   else
     SCRIPT_DIR="$HOME/nix"
 fi
+nix-shell -p git --command "git clone https://gitlab.com/librephoenix/nixos-config $SCRIPT_DIR"
 
 # Generate hardware config for new system
 sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/system/hardware-configuration.nix
@@ -26,9 +30,18 @@ sudo nix-channel --update
 # Patch flake.nix with different username/name and remove email by default
 sed -i "0,/MoonboardEnthusiast/s//$(whoami)/" $SCRIPT_DIR/flake.nix
 
+# Open up editor to manually edit flake.nix before install
+if [ -z "$EDITOR" ]; then
+    EDITOR=nano;
+fi
+$EDITOR $SCRIPT_DIR/flake.nix;
+
+# Permissions for files that should be owned by root
+sudo $SCRIPT_DIR/harden.sh $SCRIPT_DIR
+
 # Rebuild system
 sudo nixos-rebuild switch --flake $SCRIPT_DIR;
 
 # Install and build home-manager configuration
-home-manager switch --flake $SCRIPT_DIR;
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#user;
 
